a16999300fef03c3875f4d15078af86bcd31e878
Muhammed Hashir <mail2hashirpm@gmail.com>
2022-03-12 23:07:25 +0530

    c

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/secureX/precache-manifest.e5eed7a5032ec78610762c82b136944a.js"
+  "/court3.0/precache-manifest.e5eed7a5032ec78610762c82b136944a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/secureX/index.html", {
+workbox.routing.registerNavigationRoute("/court3.0/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
